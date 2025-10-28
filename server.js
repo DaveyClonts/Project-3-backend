0bd74c68274baa353bf9278a2ddee1860d55e413
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-10-28 14:06:19 -0500

    Clean up server.js

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 import routes from "./app/routes/index.js";
 import express from "express";
 import cors from "cors";
-
 import db from "./app/models/index.js";
 
 db.sequelize.sync();
-
 const app = express();
 
-// Also use the cors middleware as backup
 var corsOptions = {
     origin: "http://localhost:8081",
     credentials: true,
 };
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// Load the routes from the routes folder
 app.use("/tracker-t6", routes);
 
 // set port, listen for requests
