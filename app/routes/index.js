d1e8a66a5b48d0deb6a2d04cded8144840226a05
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-10-28 14:05:32 -0500

    Update exercise routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 
 import AuthRoutes from "./auth.routes.js";
 import UserRoutes from "./user.routes.js";
-import TutorialRoutes from "./tutorial.routes.js";
+import ExerciseRoutes from "./exercise.routes.js";
 
 const router = Router();
 
 router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
-router.use("/tutorials", TutorialRoutes);
+router.use("/exercises", ExerciseRoutes);
 
 export default router;
