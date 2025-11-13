987d8f883c7cf24af7b140be7aa18705030eee70
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-11-13 15:07:50 -0600

    Rename routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,12 +6,6 @@ import ExerciseRoutes from "./exercise.routes.js";
 
 const router = Router();
 
-router.use((req, res, next) => {
-    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-    next();
-});
-
 router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/exercises", ExerciseRoutes);
