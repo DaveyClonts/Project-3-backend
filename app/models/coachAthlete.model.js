7b494ef2cf1396cc5523824ab7b5b96a4d697469
reagancheatham <122314281+reagancheatham@users.noreply.github.com>
2025-12-01 04:03:31 -0600

    Fix coach athlete methods (#43)

--- app/models/coachAthlete.model.js
+++ app/models/coachAthlete.model.js
@@ -2,27 +2,24 @@ import { DataTypes } from "sequelize";
 import SequelizeInstance from "../config/sequelizeInstance.js";
 
 const SQLCoachAthlete = SequelizeInstance.define("coachAthlete", {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
     athleteID: {
         type: DataTypes.INTEGER,
+        primaryKey: true,
         allowNull: false,
         references: {
-            model: 'users',
-            key: 'id',
-        }
+            model: "users",
+            key: "id",
+        },
     },
     coachID: {
         type: DataTypes.INTEGER,
+        primaryKey: true,
         allowNull: false,
         references: {
-            model: 'users',
-            key: 'id',
-        }
-    }
+            model: "users",
+            key: "id",
+        },
+    },
 });
 
 export default SQLCoachAthlete;
