05b78cde168dd42722e532cb9049f0b93bcf1961
reagancheatham <122314281+reagancheatham@users.noreply.github.com>
2025-12-04 15:14:54 -0600

    Add role authentication (#53)
    * Update auth.controller.js
    
    * Update auth.controller.js
    
    * Remove unnecessary log

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -382,10 +382,11 @@ export default {
             });
     },
     authorize: async (req, res) => {
-        let user = {};
+        let user = req.body;
+        let databaseUser = {};
 
-        await findUserByID(req.body.id)
-            .then((googleUser) => (user = googleUser))
+        await findUserByID(user.id)
+            .then((googleUser) => (databaseUser = googleUser))
             .catch((err) => {
                 console.log(`Failed to find User: ${err.message}.`);
                 res.status(500).send({
@@ -393,12 +394,19 @@ export default {
                 });
             });
 
-        if (user.id === undefined) {
+        if (databaseUser.id === undefined) {
             console.log("Failed to find User.");
             res.status(500).send({ message: `Failed to find User.` });
 
             return;
+        } else if (databaseUser.role !== user.role) {
+            console.log("User role is desynced!");
+            res.status(500).send({ message: "User role is desynced." });
+
+            return;
         }
+
+        user = databaseUser;
 
         await updateGoogleToken(user)
             .then((isValid) => {
